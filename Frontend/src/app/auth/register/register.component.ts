9e077bb80552c7a5542882126d14848c8258e51c
43r63w <145778505+43r63w@users.noreply.github.com>
2024-07-24 13:22:21 +0300

    create auth and register form and user panel

---
+++ Frontend/src/app/auth/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: false,
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+register() {
+throw new Error('Method not implemented.');
+}
+}
